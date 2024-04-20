e3918a2b0852e0c4cb6935f15d4442aa0ca1c6c7
Prince Peter <princebrown0007@gmail.com>
2024-04-20 09:24:08 -0700

    0x12-javascript-warm

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,18 +1,9 @@
-lists.h
-
 #ifndef LISTS_H
 #define LISTS_H
 
 #include <stdlib.h>
 
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
+
 typedef struct listint_s
 {
 	int n;
@@ -25,4 +16,3 @@ void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
-
