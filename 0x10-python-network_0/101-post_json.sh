56f1ff957ec42ed11af821a4e2eb3fb34a14368a
PBrown0007 <princebrown0007@gmail.com>
2025-04-06 23:14:47 +0000

    alx-higher_level_programming

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that sends a JSON POST request
-curl -s -X POST "$1" -H "Content-Type: application/json" -d @"$2"
+# sends a JSON POST request to a URL, and displays the body of the response.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
